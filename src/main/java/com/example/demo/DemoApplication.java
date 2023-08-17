b96f02ce177079f7f7dfc3ca405017a54a7d558f
pmohan <p.mohan@ascendcorp.com>
2023-08-17 19:12:47 +0700

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+}
+
+@RestController
+class MyController {
+
+    private final ObjectMapper objectMapper;
+
+    MyController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping("/hello")
+    public void myMethod(@RequestBody Object body) throws JsonProcessingException {
+        String s = objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(body);
+        System.out.println(s);
+    }
 }
